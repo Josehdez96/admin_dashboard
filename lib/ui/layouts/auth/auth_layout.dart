15539c6111960e04757c23a8db96828c053037f5
josed.hernandez <josed.hernandez@pragma.com.co>
2021-08-27 07:36:08 -0500

    initial ui widgets

---
+++ lib/ui/layouts/auth/auth_layout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AuthLayout extends StatelessWidget {
+  const AuthLayout({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('Auth Layout'),
+        ),
+      ),
+    );
+  }
+}
