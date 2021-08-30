509ac78ac2c9a1b226f927b0a1ccb68c9d4f3862
josed.hernandez <josed.hernandez@pragma.com.co>
2021-08-30 08:04:18 -0500

    feat(custom_outlined_button): new button added

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:admin_dashboard/ui/buttons/custom_outlined_button.dart';
+import 'package:admin_dashboard/ui/buttons/link_text.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({ Key? key }) : super(key: key);
@@ -6,10 +8,76 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
+      margin: EdgeInsets.only(top: 100),
+      padding: EdgeInsets.symmetric(horizontal: 20),
       child: Center(
-        child: Text('LoginView'),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 370),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                  decoration: buildInputDecoration(
+                    hint: 'Ingrese su correo',
+                    label: 'Email',
+                    icon: Icons.email_outlined
+                  )
+                ),
+
+                SizedBox(height: 20),
+
+                TextFormField(
+                  obscureText: true,
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                  decoration: buildInputDecoration(
+                    hint: 'Ingrese su contraseña',
+                    label: 'Contraseña',
+                    icon: Icons.lock_outline_rounded
+                  )
+                ),
+
+                SizedBox(height: 20),
+
+                CustomOutlinedButton(
+                  onPressed: () {},
+                  text: 'Ingresar'
+                ),
+
+                SizedBox(height: 20),
+
+                LinkText(text: 'Nueva cuenta', onPressed: () {
+                  // TODO: go to register
+                })
+
+
+              ],
+            ),
+          )
+        )
       ),
     );
   }
+
+  InputDecoration buildInputDecoration({
+    required String hint,
+    required String label,
+    required IconData icon,
+  }) => InputDecoration(
+    border: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.white.withOpacity(0.3))
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.white.withOpacity(0.3))
+    ),
+    hintText: hint,
+    labelText: label,
+    hintStyle: TextStyle(color: Colors.grey),
+    labelStyle: TextStyle(color: Colors.grey),
+    prefixIcon: Icon(icon, color: Colors.grey),
+  );
 }
