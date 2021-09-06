5caa3254b61e4829351edec4aad573449fcb04be
josed.hernandez <josed.hernandez@pragma.com.co>
2021-09-06 08:00:11 -0500

    feat(sidebar): menu items

--- lib/ui/shared/sidebar.dart
+++ lib/ui/shared/sidebar.dart
@@ -18,12 +18,21 @@ class Sidebar extends StatelessWidget {
           Logo(),
           SizedBox(height: 50),
           TextSeparator(text: 'main'),
-          MenuItem(
-            text: 'Dashboard',
-            icon: Icons.compass_calibration_outlined,
-            isActive: false,
-            onPressed: () {}
-          )
+          MenuItem(text: 'Dashboard', icon: Icons.compass_calibration_outlined, onPressed: () {}),
+          MenuItem(text: 'Orders', icon: Icons.shopping_cart_outlined, onPressed: () {}),
+          MenuItem(text: 'Analytic', icon: Icons.show_chart_outlined, onPressed: () {}),
+          MenuItem(text: 'Categories', icon: Icons.layers_outlined, onPressed: () {}),
+          MenuItem(text: 'Products', icon: Icons.dashboard_outlined, onPressed: () {}),
+          MenuItem(text: 'Discount', icon: Icons.attach_money_outlined, onPressed: () {}),
+          MenuItem(text: 'Customers', icon: Icons.people_alt_outlined, onPressed: () {}),
+
+          SizedBox(height: 30),
+          TextSeparator(text: 'UI Elements'),
+          MenuItem(text: 'Icons', icon: Icons.list_alt_outlined, onPressed: () {}),
+          MenuItem(text: 'Marketing', icon: Icons.mark_email_read_outlined, onPressed: () {}),
+          MenuItem(text: 'Campaign', icon: Icons.note_add_outlined, onPressed: () {}),
+          MenuItem(text: 'Blank', icon: Icons.post_add_outlined, onPressed: () {}),
+          MenuItem(text: 'Logout', icon: Icons.exit_to_app_outlined, onPressed: () {}),
         ],
       ),
     );
