285f5b539aca089d7357446d8a3b362023276a69
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-09-13 07:09:27 -0500

    fix(not_found_page_handler): not found redirect

--- lib/router/not_found_page_handlers.dart
+++ lib/router/not_found_page_handlers.dart
@@ -1,11 +1,11 @@
+import 'package:admin_dashboard/providers/sidemenu_provider.dart';
 import 'package:admin_dashboard/ui/views/not_found_view.dart';
 import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
 
 class NotFoundPageHandlers {
-
-  static Handler notFound = Handler(
-    handlerFunc: (context, params) {
-      return NotFoundView();
-    }
-  );
+  static Handler notFound = Handler(handlerFunc: (context, params) {
+    Provider.of<SideMenuProvider>(context!, listen: false).setCurrentPageUrl('404');
+    return NotFoundView();
+  });
 }
